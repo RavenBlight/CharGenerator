c6fc31af4c4b9e2f84551e4cd4ae822da8dc2992
RavenBlight <30326422+RavenBlight@users.noreply.github.com>
2017-08-04 09:21:17 -0700

    Creation of Character Class

---
+++ Character.py
@@ -0,0 +1,3 @@
+# this is going to be the creation of the Character class
+# within this class will be a few different types of characters, but they will all have some similarities...
+# name, race, age, height, weight, class, stat block,
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CharGenerator
 This is a Character Generator side project that is mainly for learning python
+
