52bf4fbe92eeb45a5672fdb578dc061959f8cf81
RavenBlight <30326422+RavenBlight@users.noreply.github.com>
2018-01-23 10:29:23 -0800

    Creation of Character Class

--- Character.py
+++ Character.py
@@ -1,3 +1,7 @@
-# this is going to be the creation of the Character class
-# within this class will be a few different types of characters, but they will all have some similarities...
+# this is going to be the creation of the 'Character' class
+# within this class will be a few different types of 'characters', but they will all have some similarities...
 # name, race, age, height, weight, class, stat block,
+
+class PlayerChar:
+
+
